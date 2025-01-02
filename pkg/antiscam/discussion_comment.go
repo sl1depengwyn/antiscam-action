f6e1830f42050e3488bc44a21cdbb5ffb2181e58
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2025-01-02 10:38:28 +0300

    Update discussion_comment.go

--- pkg/antiscam/discussion_comment.go
+++ pkg/antiscam/discussion_comment.go
@@ -8,37 +8,43 @@ import (
 )
 
 func (a *Antiscam) ProcessDiscussionComment(payload []byte) error {
-	var discussion_comment github.DiscussionComment
-	var discussion github.Discussion
-
-	// github.Event
-	if err := json.Unmarshal(payload, &discussion_comment); err != nil {
-		return err
-	}
-
-	if err := json.Unmarshal(payload, &discussion); err != nil {
-		return err
-	}
+	var discussion_event github.DiscussionEvent
+	var team_event github.TeamEvent
+	var issue_comment github.IssueCommentEvent
 
 	fmt.Printf("raw event payload: %s\n", payload)
 
-	fmt.Printf("discussion comment: %s\n", discussion_comment)
+	if err := json.Unmarshal(payload, &discussion_event); err != nil {
+		return err
+	}
 
-	fmt.Printf("discussion: %s\n", github.Stringify(discussion))
+	fmt.Printf("discussion event: %s\n", github.Stringify(discussion_event))
+
+	if err := json.Unmarshal(payload, &team_event); err != nil {
+		return err
+	}
+
+	fmt.Printf("team event: %s\n", github.Stringify(team_event))
+
+	if err := json.Unmarshal(payload, &issue_comment); err != nil {
+		return err
+	}
+
+	fmt.Printf("issue comment: %s\n", github.Stringify(issue_comment))
 
 	// discussion_comment.get
 
-	var detections []Detection
-	detections = append(detections, checkComment(discussion_comment.GetBody())...)
+	// var detections []Detection
+	// detections = append(detections, checkComment(discussion_comment.GetBody())...)
 
 	// body := fmt.Sprintf("@%s The previous user tried to scam you by providing a fake support link. Don't interact with it.\n", discussion_comment.GetAuthor().GetLogin())
 
-	for _, detection := range detections {
-		fmt.Printf("Detected scam in %s: %s\n", detection.Location, detection.DebugInfo)
-	}
+	// for _, detection := range detections {
+	// 	fmt.Printf("Detected scam in %s: %s\n", detection.Location, detection.DebugInfo)
+	// }
 
 	// if len(detections) > 0 {
-		// a.client.Teams.DeleteCommentByID(a.ctx, )
+	// a.client.Teams.DeleteCommentByID(a.ctx, )
 
 	// 	a.client.Issues.DeleteComment(
 	// 		a.ctx,
