c37a8e57c053699bcd3a944e72a0af8f38005baf
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2025-01-21 10:14:27 +0300

    Update discussion_comment.go

--- pkg/antiscam/discussion_comment.go
+++ pkg/antiscam/discussion_comment.go
@@ -9,61 +9,57 @@ import (
 
 func (a *Antiscam) ProcessDiscussionComment(payload []byte) error {
 	var event map[string]interface{}
-
 	if err := json.Unmarshal(payload, &event); err != nil {
 		return err
 	}
 
-	var mutation struct {
-		DeleteDiscussionComment struct {
-			ClientMutationID *string
-		} `graphql:"deleteDiscussionComment(input: $input)"`
+	var detections []Detection
+	detections = append(detections, checkComment(event["comment"].(map[string]interface{})["body"].(string))...)
+
+	for _, detection := range detections {
+		fmt.Printf("Detected scam in %s: %s\n", detection.Location, detection.DebugInfo)
 	}
 
-	input := githubv4.DeleteDiscussionCommentInput{
-		ID: githubv4.ID(event["comment"].(map[string]interface{})["node_id"].(string)),
+	if len(detections) > 0 {
+		var comment_deletion struct {
+			DeleteDiscussionComment struct {
+				ClientMutationID *string
+			} `graphql:"deleteDiscussionComment(input: $input)"`
+		}
+
+		comment_deletion_input := githubv4.DeleteDiscussionCommentInput{
+			ID: githubv4.ID(event["comment"].(map[string]interface{})["node_id"].(string)),
+		}
+
+		comment_deletion_err := a.graphql_client.Mutate(a.ctx, &comment_deletion, comment_deletion_input, nil)
+
+		if comment_deletion_err != nil {
+			return comment_deletion_err
+		}
+
+		var comment_addition struct {
+			AddDiscussionComment struct {
+				Comment struct {
+					ID   githubv4.ID
+					Body githubv4.String
+					URL  githubv4.URI
+				}
+				ClientMutationID *string
+			} `graphql:"addDiscussionComment(input: $input)"`
+		}
+
+		body := fmt.Sprintf("@%s The previous user tried to scam you by providing a fake support link. Don't interact with it.\n", event["comment"].(map[string]interface{})["user"].(map[string]interface{})["login"].(string))
+
+		comment_addition_input := githubv4.AddDiscussionCommentInput{
+			DiscussionID: githubv4.ID(event["discussion"].(map[string]interface{})["node_id"].(string)),
+			Body:         githubv4.String(body),
+		}
+
+		comment_addition_err := a.graphql_client.Mutate(a.ctx, &comment_addition, comment_addition_input, nil)
+		if comment_addition_err != nil {
+			return comment_addition_err
+		}
 	}
-
-	err := a.graphql_client.Mutate(a.ctx, &mutation, input, nil)
-
-	if err != nil {
-		fmt.Println("Error deleting discussion comment: %v", err)
-	}
-
-	fmt.Println("Discussion comment deleted successfully!")
-	// discussion_comment.get
-
-	// var detections []Detection
-	// detections = append(detections, checkComment(discussion_comment.GetBody())...)
-
-	// body := fmt.Sprintf("@%s The previous user tried to scam you by providing a fake support link. Don't interact with it.\n", discussion_comment.GetAuthor().GetLogin())
-
-	// for _, detection := range detections {
-	// 	fmt.Printf("Detected scam in %s: %s\n", detection.Location, detection.DebugInfo)
-	// }
-
-	// if len(detections) > 0 {
-	// a.client.Teams.DeleteCommentByID(a.ctx, )
-
-	// 	a.client.Issues.DeleteComment(
-	// 		a.ctx,
-	// 		event.GetRepo().GetOwner().GetLogin(),
-	// 		event.GetRepo().GetName(),
-	// 		event.GetComment().GetID(),
-	// 	)
-
-	// 	if _, _, err := a.client.Issues.CreateComment(
-	// 		a.ctx,
-	// 		event.GetRepo().GetOwner().GetLogin(),
-	// 		event.GetRepo().GetName(),
-	// 		event.GetIssue().GetNumber(),
-	// 		&github.IssueComment{
-	// 			Body: &body,
-	// 		},
-	// 	); err != nil {
-	// 		return err
-	// 	}
-	// }
 
 	return nil
 }
