e35d880e333c70d8950494562dbca90f5e09f9fd
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2025-01-21 10:22:20 +0300

    Update discussion_comment.go

--- pkg/antiscam/discussion_comment.go
+++ pkg/antiscam/discussion_comment.go
@@ -3,63 +3,85 @@ package antiscam
 import (
 	"encoding/json"
 	"fmt"
-	// "github.com/google/go-github/v50/github"
 	"github.com/shurcooL/githubv4"
 )
 
+type DiscussionCommentPayload struct {
+	Comment struct {
+		NodeID string `json:"node_id"`
+		Body   string `json:"body"`
+		User   struct {
+			Login string `json:"login"`
+		} `json:"user"`
+	} `json:"comment"`
+	Discussion struct {
+		NodeID string `json:"node_id"`
+	} `json:"discussion"`
+}
+
+type deleteCommentMutation struct {
+	DeleteDiscussionComment struct {
+		ClientMutationID *string
+	} `graphql:"deleteDiscussionComment(input: $input)"`
+}
+
+type addCommentMutation struct {
+	AddDiscussionComment struct {
+		Comment struct {
+			ID   githubv4.ID
+			Body githubv4.String
+			URL  githubv4.URI
+		}
+		ClientMutationID *string
+	} `graphql:"addDiscussionComment(input: $input)"`
+}
+
 func (a *Antiscam) ProcessDiscussionComment(payload []byte) error {
-	var event map[string]interface{}
+	var event DiscussionCommentPayload
 	if err := json.Unmarshal(payload, &event); err != nil {
-		return err
+		return fmt.Errorf("failed to unmarshal payload: %w", err)
 	}
 
-	var detections []Detection
-	detections = append(detections, checkComment(event["comment"].(map[string]interface{})["body"].(string))...)
+	detections := checkComment(event.Comment.Body)
+	if len(detections) == 0 {
+		return nil
+	}
 
 	for _, detection := range detections {
 		fmt.Printf("Detected scam in %s: %s\n", detection.Location, detection.DebugInfo)
 	}
 
-	if len(detections) > 0 {
-		var comment_deletion struct {
-			DeleteDiscussionComment struct {
-				ClientMutationID *string
-			} `graphql:"deleteDiscussionComment(input: $input)"`
-		}
+	if err := a.deleteDiscussionScamComment(event.Comment.NodeID); err != nil {
+		return fmt.Errorf("failed to delete scam comment: %w", err)
+	}
 
-		comment_deletion_input := githubv4.DeleteDiscussionCommentInput{
-			ID: githubv4.ID(event["comment"].(map[string]interface{})["node_id"].(string)),
-		}
-
-		comment_deletion_err := a.graphql_client.Mutate(a.ctx, &comment_deletion, comment_deletion_input, nil)
-
-		if comment_deletion_err != nil {
-			return comment_deletion_err
-		}
-
-		var comment_addition struct {
-			AddDiscussionComment struct {
-				Comment struct {
-					ID   githubv4.ID
-					Body githubv4.String
-					URL  githubv4.URI
-				}
-				ClientMutationID *string
-			} `graphql:"addDiscussionComment(input: $input)"`
-		}
-
-		body := fmt.Sprintf("@%s The previous user tried to scam you by providing a fake support link. Don't interact with it.\n", event["comment"].(map[string]interface{})["user"].(map[string]interface{})["login"].(string))
-
-		comment_addition_input := githubv4.AddDiscussionCommentInput{
-			DiscussionID: githubv4.ID(event["discussion"].(map[string]interface{})["node_id"].(string)),
-			Body:         githubv4.String(body),
-		}
-
-		comment_addition_err := a.graphql_client.Mutate(a.ctx, &comment_addition, comment_addition_input, nil)
-		if comment_addition_err != nil {
-			return comment_addition_err
-		}
+	if err := a.addWarningDiscussionComment(event.Discussion.NodeID, event.Comment.User.Login); err != nil {
+		return fmt.Errorf("failed to add warning comment: %w", err)
 	}
 
 	return nil
+}
+
+func (a *Antiscam) deleteDiscussionScamComment(commentID string) error {
+	var mutation deleteCommentMutation
+	input := githubv4.DeleteDiscussionCommentInput{
+		ID: githubv4.ID(commentID),
+	}
+
+	return a.graphql_client.Mutate(a.ctx, &mutation, input, nil)
+}
+
+func (a *Antiscam) addWarningDiscussionComment(discussionID string, userLogin string) error {
+	var mutation addCommentMutation
+	body := fmt.Sprintf(
+		"@%s The previous user tried to scam you by providing a fake support link. Don't interact with it.\n",
+		userLogin,
+	)
+
+	input := githubv4.AddDiscussionCommentInput{
+		DiscussionID: githubv4.ID(discussionID),
+		Body:         githubv4.String(body),
+	}
+
+	return a.graphql_client.Mutate(a.ctx, &mutation, input, nil)
 }
